44c4bbfec44c95f677007549a1803d72d2fcf2af
k.kaloshin <k.kaloshin@bars.group>
2022-02-21 00:24:07 +0300

    День 3. Добавлены задания

---
+++ tasks/comprehensions/task_1/tests.py
@@ -0,0 +1,23 @@
+import unittest
+
+from tasks.comprehensions.task_1.implementation import (
+    flatten_list,
+)
+
+
+class MyUnitTest(unittest.TestCase):
+
+    def test_flatten_list(self):
+        matrix = [
+            [0, 0, 0],
+            [1, 1, 1],
+            [2, 2, 2],
+        ]
+
+        flat = flatten_list(matrix)
+
+        self.assertListEqual(flat, [0, 0, 0, 1, 1, 1, 2, 2, 2])
+
+
+if __name__ == '__main__':
+    unittest.main()
