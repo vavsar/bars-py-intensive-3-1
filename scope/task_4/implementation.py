762459ea81fb41e91f2cc288c3ca9b8765b20f03
Dmitriy Chernyshov <dechernyshov@bars.group>
2022-02-20 16:50:08 +0300

    Добавлены задания для flow_control и scope

---
+++ scope/task_4/implementation.py
@@ -0,0 +1,14 @@
+def bad_open(file_path, mode):
+    """Некорректная функция открытия файла"""
+    raise Exception
+
+
+def open_and_close_file(file_path):
+    """Открывет и закрывает файл
+
+    Args:
+        file_path: путь до файла
+    """
+    open = bad_open
+    f = open(file_path, 'r')
+    f.close()
